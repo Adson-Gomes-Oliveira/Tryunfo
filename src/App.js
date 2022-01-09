fc129b6a65a9eebd965d3091fb4bce78b58eecb3
Adson-Gomes-Oliveira <adsongoliveira2021@gmail.com>
2022-01-09 05:02:09 -0300

    Quinto Requisito concluído!.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.handleInput = this.handleInput.bind(this);
-    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
-
     this.state = {
       nameCard: '',
       description: '',
@@ -17,21 +14,52 @@ class App extends React.Component {
       attr1: '',
       attr2: '',
       attr3: '',
-      rarity: '',
+      rarity: 'normal',
       superTrunfo: false,
       isSaveButtonDisabled: true,
     };
   }
 
-  handleInput(event) {
+  handleInput = (event) => {
     const { value, id, checked, type } = event.target;
-
     const typeOfValue = type === 'checkbox' ? checked : value;
 
-    this.setState({ [id]: typeOfValue });
+    this.setState({ [id]: typeOfValue }, () => this.validation());
   }
 
-  onSaveButtonClick() {
+  validateAttribute = (string) => {
+    const maxAttr = 90;
+    const minAttr = 0;
+    string = parseInt(string, 10);
+    string = string >= minAttr && string <= maxAttr;
+    return string;
+  }
+
+  validateFullStrenght = (attribute1, attribute2, attribute3, rule) => {
+    attribute1 = parseInt(attribute1, 10);
+    attribute2 = parseInt(attribute2, 10);
+    attribute3 = parseInt(attribute3, 10);
+    const fullAttr = attribute1 + attribute2 + attribute3;
+    console.log(fullAttr);
+    const valid = fullAttr <= rule;
+    return valid;
+  }
+
+  validation = () => {
+    const totalAttr = 210;
+    const { attr1, attr2, attr3 } = this.state;
+    const stateList = Object.values(this.state);
+    this.setState({ isSaveButtonDisabled: true });
+    if (!stateList.includes('')
+    && this.validateAttribute(attr1) === true
+    && this.validateAttribute(attr2) === true
+    && this.validateAttribute(attr3) === true
+    && this.validateFullStrenght(attr1, attr2, attr3, totalAttr) === true) {
+      this.setState({ isSaveButtonDisabled: false });
+    }
+  }
+
+  onSaveButtonClick = () => {
   }
 
   render() {
